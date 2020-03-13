8e2a9c32add0770d761839e4fd7da18968f84d9f
Darin Morrison <darinmorrison@users.noreply.github.com>
2020-03-12 20:03:15 -0600

    Define project

---
+++ src/extension.ts
@@ -0,0 +1,9 @@
+import * as vscode from "vscode";
+
+export function activate(_: vscode.ExtensionContext): Promise<void> {
+  return Promise.resolve();
+}
+
+export function deactivate(): Promise<void> | undefined {
+  return Promise.resolve();
+}
